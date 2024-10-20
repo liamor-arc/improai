3e4ac63144ffbfb216f9a7dc94409065de7ee681
Tristan Verheecke <tristanverheecke@liamor-arc.be>
2024-10-20 21:32:36 +0200

    Requirements

--- commands/install.ps1
+++ commands/install.ps1
@@ -5,10 +5,6 @@ python -m venv .venv
 python -m pip install --upgrade pip
 
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
-pip install openai
-pip install pyyaml
-pip install azure-cognitiveservices-speech
-pip install transformers sentencepiece datasets[audio]
-pip install pyaudio
+pip install -r requirements.txt
 
 deactivate
